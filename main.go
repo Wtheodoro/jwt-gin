e0bef2a95f9d72e3612706ffb9a5d294b1b0cb9e
Walison Theodoro <theodoro.walison@gmail.com>
2023-08-25 17:19:44 -0300

    init: add project base

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	public := r.Group("/api")
+
+	public.POST("/register", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": "This is the register endpoint!"})
+	})
+
+	r.Run(":8080")
+}
