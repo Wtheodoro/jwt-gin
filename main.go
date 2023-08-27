7a5a14b9a68dc850912a19df642c0655dc0101a9
Walison Theodoro <theodoro.walison@gmail.com>
2023-08-27 20:25:44 -0300

    feat: add register api

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"balance_bay/src/controllers"
+	"balance_bay/src/models"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	models.ConnectDataBase()
+
 	r := gin.Default()
 
 	public := r.Group("/api")
