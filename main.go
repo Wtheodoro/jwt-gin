b2429fd46c3260932b793f8a3900f69d87db985b
Walison Theodoro <theodoro.walison@gmail.com>
2023-09-02 22:05:47 -0300

    fix: add cors middleware

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"balance_bay/src/controllers"
 	"balance_bay/src/middlewares"
 	"balance_bay/src/models"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,6 +13,7 @@ func main() {
 	models.ConnectDataBase()
 
 	r := gin.Default()
+	r.Use(CORSMiddleware())
 
 	public := r.Group("/api")
 
@@ -23,4 +25,24 @@ func main() {
 	protected.GET("/user",controllers.CurrentUser)
 
 	r.Run(":8080")
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		fmt.Println(c.Request.Method)
+
+		if c.Request.Method == "OPTIONS" {
+			fmt.Println("OPTIONS")
+			c.AbortWithStatus(200)
+		} else {
+			c.Next()
+		}
+	}
 }
